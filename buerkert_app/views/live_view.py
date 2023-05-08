1c2dfcf60bf768c2980f72e48b5fce1f6ca1efac
Tony <anton1.maier@student.hswt.de>
2023-05-08 21:40:27 +0200

    updatet pipfile, better error messages

--- buerkert_app/views/live_view.py
+++ buerkert_app/views/live_view.py
@@ -2,6 +2,7 @@ import asyncio
 
 from asyncua import Client
 
+from django.contrib import messages
 from django.shortcuts import render
 from django.views import View
 
@@ -10,31 +11,40 @@ class LiveView(View):
     url = "opc.tcp://localhost:4840/freeopcua/server/"
     namespace = "http://examples.freeopcua.github.io"
 
-    async def get_data(self):
-        async with Client(url=self.url) as client:
-            loop = asyncio.get_event_loop()
-            # Find the namespace index
-            nsidx = await client.get_namespace_index(self.namespace)
+    async def get_data(self, request):
+        exc = ""
+        try:
+            async with Client(url=self.url) as client:
+                loop = asyncio.get_event_loop()
+                # Find the namespace index
+                nsidx = await client.get_namespace_index(self.namespace)
 
-            # Get the variable node for read / write
-            var = await client.nodes.root.get_child(["0:Objects", f"{nsidx}:MyObject", f"{nsidx}:MyVariable"])
-            var1 = await client.nodes.root.get_child(["0:Objects", f"{nsidx}:MyObject", f"{nsidx}:MyVariable1"])
-            var2 = await client.nodes.root.get_child(["0:Objects", f"{nsidx}:MyObject", f"{nsidx}:MyVariable2"])
-            var3 = await client.nodes.root.get_child(["0:Objects", f"{nsidx}:MyObject", f"{nsidx}:MyVariable3"])
-            value = await var.read_value()
-            value1 = await var1.read_value()
-            value2 = await var2.read_value()
-            value3 = await var3.read_value()
+                # Get the variable node for read / write
+                var = await client.nodes.root.get_child(["0:Objects", f"{nsidx}:MyObject", f"{nsidx}:MyVariable"])
+                var1 = await client.nodes.root.get_child(["0:Objects", f"{nsidx}:MyObject", f"{nsidx}:MyVariable1"])
+                var2 = await client.nodes.root.get_child(["0:Objects", f"{nsidx}:MyObject", f"{nsidx}:MyVariable2"])
+                var3 = await client.nodes.root.get_child(["0:Objects", f"{nsidx}:MyObject", f"{nsidx}:MyVariable3"])
+                value = await var.read_value()
+                value1 = await var1.read_value()
+                value2 = await var2.read_value()
+                value3 = await var3.read_value()
 
-            return [value, value1, value2, value3]
+                return [value, value1, value2, value3], []
+        except TimeoutError:
+            exc = f"TimeoutError: Can´t Connect to OPC-UA Server {self.url}"
+            return [], [exc,]
+        except Exception as e:
+            exc = e
+            return [], [exc,]
+
 
     def get(self, request):
         if request.htmx:
             loop = asyncio.new_event_loop()
             asyncio.set_event_loop(loop)
             loop = asyncio.get_event_loop()
-            values = loop.run_until_complete(self.get_data())
+            values, opc_messages = loop.run_until_complete(self.get_data(request))
             loop.close()
-            context = {"values": values}
+            context = {"values": values, "opc_messages": opc_messages}
             return render(request, "snippets/data_values.html", context)
         return render(request, "buerkert_app/live_view.html")
