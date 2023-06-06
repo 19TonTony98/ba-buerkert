d48cf846a77e906c63093c60ad92280a75801725
hswts117912 <maier.anton@aol.com>
2023-06-06 18:35:54 +0200

    collapse table

--- buerkert_app/dashes/batch_dash.py
+++ buerkert_app/dashes/batch_dash.py
@@ -2,7 +2,7 @@ import random
 import uuid
 from math import ceil as up
 
-from dash import dcc, html, Input, Output, dash_table
+from dash import dcc, html, Input, Output, dash_table, State
 from django_plotly_dash import DjangoDash
 import dash_bootstrap_components as dbc
 import plotly.express as px
@@ -24,17 +24,22 @@ class BatchDash:
                 labelStyle={"margin": "10px"}
             ),
             dbc.Spinner(dcc.Graph(id="graph"), fullscreen=True, color="success"),
-            html.Div([dbc.Button("Export Table", id="export_table", className="me-md-2")],
+            html.Div([dbc.Button("Zeige Tabelle", id="collapse_table", className="me-md-2", n_clicks=0),
+                      dbc.Button("Export Tabelle", id="export_table", className="me-md-2")],
                      className="d-grid gap-2 d-md-flex justify-content-md-end mb-2"),
-            dash_table.DataTable(df.to_dict('records'), id='batch_table', filter_action='native', export_format='xlsx',
-                                 page_size=page_size,
-                                 columns=[{"name": "Sensor-ID", "id": "sensor_id"},
-                                          {"name": "Field", "id": "_field"},
-                                          {"name": "Time", "id": "_time"},
-                                          {"name": "Value", "id": "_value"}]),
-            dbc.Pagination(id="pagination", max_value=up(df.shape[0] / page_size),
-                           first_last=True, previous_next=True, fully_expanded=False,
-                           className="justify-content-center mt-2"),
+            dbc.Collapse([dash_table.DataTable(df.to_dict('records'), id='batch_table', filter_action='native',
+                                               export_format='xlsx',
+                                               page_size=page_size,
+                                               columns=[{"name": "Sensor-ID", "id": "sensor_id"},
+                                                        {"name": "Field", "id": "_field"},
+                                                        {"name": "Time", "id": "_time"},
+                                                        {"name": "Value", "id": "_value"}]),
+                          dbc.Pagination(id="pagination", max_value=up(df.shape[0] / page_size),
+                                         first_last=True, previous_next=True, fully_expanded=False,
+                                         className="justify-content-center mt-2"),
+                          ],
+                         id="collapseTable", is_open=False
+                         )
         ])
 
         app.clientside_callback(
@@ -65,12 +70,13 @@ class BatchDash:
             for i, value in enumerate(values, 1):
                 v_name = value if len(values) > 1 else ""
                 for group_name, group_df in df.loc[df[check_col] == value].groupby(color):
-                    fig.add_trace(go.Scatter(x=group_df[x], y=group_df[y], name=f"{group_name}({v_name})", yaxis=f"y{i}"))
+                    fig.add_trace(
+                        go.Scatter(x=group_df[x], y=group_df[y], name=f"{group_name}({v_name})", yaxis=f"y{i}"))
                 f_col = px.colors.sequential.Rainbow[i]
                 layout_dict[f"yaxis{i}"] = {"title": v_name,
                                             "titlefont": {"color": f_col},
                                             "tickfont": {"color": f_col},
-                                            "side": "left" if i % 2 else "right",}
+                                            "side": "left" if i % 2 else "right", }
                 if i > 1:
                     layout_dict[f"yaxis{i}"].update({"overlaying": "y"})
             fig.update_layout(**layout_dict)
@@ -89,3 +95,14 @@ class BatchDash:
         )
         def change_pagingation(values):
             return up(df[df["_field"].isin(values)].shape[0] / page_size)
+
+        @app.callback(
+            [Output("collapseTable", "is_open"), Output("collapse_table", "children")],
+            [Input("collapse_table", "n_clicks")],
+            [State("collapseTable", "is_open")],
+        )
+        def toggle_collapse(n, is_open):
+            label = "Zeige Tabelle" if is_open else "Verberge Tabelle"
+            if n:
+                return not is_open, label
+            return is_open, label
