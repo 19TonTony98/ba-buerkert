21c9f4d14f7282182ede817473a918dc6f42a0ca
hswts117912 <maier.anton@aol.com>
2023-06-12 17:37:31 +0200

    upload funktionsschema

--- buerkert_app/views/live_view.py
+++ buerkert_app/views/live_view.py
@@ -1,6 +1,9 @@
 import asyncio
 
 from django.contrib import messages
+from django import forms
+from django.core.exceptions import ValidationError
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 
@@ -30,5 +33,34 @@ class LiveView(View):
             batch, running = is_container_running()
         except Exception as e:
             messages.error(request, e)
-        context.update({"batch": batch, "running": running})
+        form = UploadFileForm()
+        context.update({"batch": batch, "running": running, "form": form})
         return render(request, "buerkert_app/live_view.html", context)
+
+    def post(self, request):
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES["file"])
+            messages.success(request, "Datei erfolgreich hochgeladen")
+        else:
+            for error in form.errors['file']:
+                messages.error(request, error)
+        return self.get(request)
+
+
+class UploadFileForm(forms.Form):
+    file = forms.FileField(label="Neues Funktionsschema hochladen")
+
+    def clean_file(self):
+        data = self.cleaned_data['file']
+        content_type = data.content_type
+        if content_type == 'image/jpeg':
+            return data
+        else:
+            raise ValidationError('Ungültiger Datentyp, Akzeptierte Datentypen: .jpg')
+
+
+def handle_uploaded_file(f):
+    with open("buerkert_app/static/buerkert_app/img/buerkert_funktionsschema.jpg", "wb+") as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
