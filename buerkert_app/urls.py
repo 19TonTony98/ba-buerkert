52d71da141eb7b9b04a375e77f91bd7f2ade3f77
hswts117912 <maier.anton@aol.com>
2023-06-13 15:23:07 +0200

    added documents

--- buerkert_app/urls.py
+++ buerkert_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, re_path
 
 from buerkert_app.views.batch_view import BatchView
+from buerkert_app.views.document_view import DocumentView
 from buerkert_app.views.index import Index
 from buerkert_app.views.live_view import LiveView
 from buerkert_app.views.start_view import StartView
@@ -9,5 +10,6 @@ urlpatterns = [
     path("", Index.as_view(), name="index"),
     re_path(r"batch/(?P<batch_id>\w+)", BatchView.as_view(), name='batch'),
     path(r"live/", LiveView.as_view(), name='live'),
-    path(r"start/", StartView.as_view(), name='start')
+    path(r"start/", StartView.as_view(), name='start'),
+    path(r"documents/", DocumentView.as_view(), name='documents')
 ]
---
+++ buerkert_app/views/document_view.py
@@ -0,0 +1,17 @@
+import os
+
+from django.http import FileResponse
+from django.shortcuts import render
+from django.views import View
+
+
+class DocumentView(View):
+    def get(self, request):
+        document_root = os.path.join("res", "documents")
+        if document := request.GET.get("document"):
+            filename = os.path.join(document_root, document)
+            response = FileResponse(open(filename, 'rb'))
+            return response
+        documents = [document for document in os.listdir(document_root)]
+        context = {"documents": documents}
+        return render(request, 'buerkert_app/document_view.html', context)
